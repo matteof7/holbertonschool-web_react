a0bcbb022cb44b927432d49bc8fa8238c8a31125
matteof7 <matteofoti741@gmail.com>
2025-05-27 10:36:13 +0200

    task3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -32,3 +32,12 @@ const director1: Director = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+console.log(printTeacher("John", "Doe"));
+console.log(printTeacher("Alice", "Smith"));
